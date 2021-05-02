b3f588cbf2dd434b7fee957e4fae95f728f2e0c6
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-02 19:32:24 -0400

    feat: build a server route that retrieves a list of movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import './App.css'
 
 function App() {
-  return <div>The Shoppies</div>
+  const fetchData = () => {
+    fetch('/getmovies', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data)
+      })
+      .catch((err) => console.log(err))
+  }
+  return (
+    <>
+      <div>The Shoppies</div>
+      <button onClick={fetchData}>Fetch Data</button>
+    </>
+  )
 }
 
 export default App
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3001
 const app = express()
+const fetch = require('node-fetch')
 
 // Define middleware here
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
@@ -12,6 +15,16 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Define API routes here
+app.get('/getmovies', (req, res) => {
+  fetch(
+    `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=blade+runner&type=movie&page=1`
+  )
+    .then((res) => res.json())
+    .then((data) => res.send({ data: data }))
+    .catch((err) => {
+      res.status(500).json(err)
+    })
+})
 
 // Send every other request to the React app
 // Define any API routes before this runs
