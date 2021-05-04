54c0555255bd069013283c018aa46eafd28e3cbc
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-03 20:08:42 -0400

    feat: poster poster for each search result. If not available, render a placeholder

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './SearchResults.css'
 
 const Searchbar = ({ props: { searchResults, setSearchResults } }) => {
   return (
@@ -7,8 +8,23 @@ const Searchbar = ({ props: { searchResults, setSearchResults } }) => {
         searchResults.Response !== 'False' &&
         searchResults.Search.map((item) => {
           return (
-            <div key={item.imdbID + Math.floor(Math.random() * 100)}>
-              {`${item.Title}, ${item.Year}`}
+            <div className="searchResult">
+              <img
+                className="resultPoster"
+                src={
+                  item.Poster !== 'N/A'
+                    ? item.Poster
+                    : 'https://via.placeholder.com/150x222.png?text=N/A'
+                }
+                alt={item.Title}
+              />
+              <div>
+                <div key={item.imdbID + Math.floor(Math.random() * 100)}>
+                  {`${item.Title}`}
+                </div>
+                <div>{item.Year}</div>
+                <button>Nominate</button>
+              </div>
             </div>
           )
         })}
