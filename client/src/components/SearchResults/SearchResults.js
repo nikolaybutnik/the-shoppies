b243aa638a0cfd5949aadc2d5506bfb8e1321ef1
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-03 20:44:43 -0400

    style: general styling of search results

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,29 +1,37 @@
 import React from 'react'
 import './SearchResults.css'
 
-const Searchbar = ({ props: { searchResults, setSearchResults } }) => {
+const Searchbar = ({ props: { searchResults } }) => {
   return (
     <>
       {searchResults &&
         searchResults.Response !== 'False' &&
         searchResults.Search.map((item) => {
           return (
-            <div className="searchResult">
+            <div
+              id={item.imdbID}
+              className="searchResult"
+              key={item.imdbID + Math.floor(Math.random() * 100)}
+            >
               <img
                 className="resultPoster"
                 src={
                   item.Poster !== 'N/A'
                     ? item.Poster
-                    : 'https://via.placeholder.com/150x222.png?text=N/A'
+                    : 'https://via.placeholder.com/125x185.png?text=N/A'
                 }
                 alt={item.Title}
               />
               <div>
-                <div key={item.imdbID + Math.floor(Math.random() * 100)}>
-                  {`${item.Title}`}
-                </div>
-                <div>{item.Year}</div>
-                <button>Nominate</button>
+                <h3>{`${item.Title}`}</h3>
+                <h5>{`Year released: ${item.Year}`}</h5>
+                <button
+                  onClick={(e) =>
+                    console.log(e.target.parentNode.parentNode.id)
+                  }
+                >
+                  Nominate
+                </button>
               </div>
             </div>
           )
