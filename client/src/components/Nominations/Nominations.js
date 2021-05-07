e36d4c47e17783977e297a34e16dd6b0a46d1d32
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-07 16:27:44 -0400

    feat: create a server call button that sends movie ID to be deleted from DB

--- client/src/components/Nominations/Nominations.js
+++ client/src/components/Nominations/Nominations.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import './Nominations.css'
 
-const Nominations = ({ props: { existingNominations } }) => {
-  console.log(existingNominations)
+const Nominations = ({
+  props: { existingNominations, setExistingNominations },
+}) => {
+  const handleDelete = (e) => {
+    const [, deleteID] = e.target.parentNode.parentNode.id.split('+')
+    console.log(deleteID)
+    fetch(`/removenomination/${deleteID}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data)
+        // receive remaining nominations and set as state
+      })
+      .catch((err) => console.log(err))
+  }
+
   return (
     <div>
       {existingNominations &&
         existingNominations.map((item) => {
           return (
-            <div key={item.imdbID} className="nomination">
+            <div
+              id={`nomination+${item.imdbID}`}
+              key={item.imdbID}
+              className="nomination"
+            >
               <img
                 className="nominationPoster"
                 src={item.poster}
@@ -17,7 +40,7 @@ const Nominations = ({ props: { existingNominations } }) => {
               <div>
                 <h3>{`${item.title}`}</h3>
                 <h5>{`Year released: ${item.year}`}</h5>
-                <button>Delete</button>
+                <button onClick={(e) => handleDelete(e)}>Delete</button>
               </div>
             </div>
           )
