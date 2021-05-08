16314ee989bd8e91dbab35c5e0ec75617ac35557
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-07 21:04:29 -0400

    feat: enable displaying movie plot on nominations

--- client/src/components/Nominations/Nominations.js
+++ client/src/components/Nominations/Nominations.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Collapsible from 'react-collapsible'
 import './Nominations.css'
 
 const Nominations = ({
@@ -20,6 +21,28 @@ const Nominations = ({
       .catch((err) => console.log(err))
   }
 
+  const fetchPlot = (id) => {
+    if (
+      document.getElementById(`nominationplot+${id}`).innerHTML === 'Loading...'
+    ) {
+      fetch(`/getplot/${id}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            document.getElementById(`nominationplot+${id}`).innerHTML =
+              data.data
+          }
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+
   return (
     <div>
       {existingNominations &&
@@ -38,6 +61,13 @@ const Nominations = ({
               <div>
                 <h3>{`${item.title}`}</h3>
                 <h5>{`Year released: ${item.year}`}</h5>
+                <Collapsible
+                  trigger="View Plot"
+                  triggerWhenOpen="Collapse Plot"
+                  onOpen={() => fetchPlot(item.imdbID)}
+                >
+                  <p id={`nominationplot+${item.imdbID}`}>Loading...</p>
+                </Collapsible>
                 <button onClick={(e) => handleDelete(e)}>Delete</button>
               </div>
             </div>
