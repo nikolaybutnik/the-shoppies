577f0164d044a38efc41df70c6076d776f9f49df
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-08 20:29:48 -0400

    style: add styling for nominated movies

--- client/src/components/Nominations/Nominations.js
+++ client/src/components/Nominations/Nominations.js
@@ -8,20 +8,20 @@ const Nominations = ({
   props: { existingNominations, setExistingNominations },
 }) => {
   const handleDelete = (e) => {
-    const [, deleteID] = e.target.parentNode.parentNode.id.split('+')
+    const [, deleteID] = e.target.parentNode.id.split('+')
     removeNomination(deleteID, setExistingNominations)
   }
 
-  const fetchPlot = (id) => {
-    if (
-      document.getElementById(`nominationplot+${id}`).innerHTML === 'Loading...'
-    ) {
-      getPlot(id, 'nomination')
-    }
-  }
+  // const fetchPlot = (id) => {
+  //   if (
+  //     document.getElementById(`nominationplot+${id}`).innerHTML === 'Loading...'
+  //   ) {
+  //     getPlot(id, 'nomination')
+  //   }
+  // }
 
   return (
-    <div>
+    <div className="allNominations">
       {existingNominations &&
         existingNominations.map((item) => {
           return (
@@ -30,23 +30,22 @@ const Nominations = ({
               key={item.imdbID}
               className="nomination"
             >
-              <img
-                className="nominationPoster"
-                src={item.poster}
-                alt={item.title}
-              />
-              <div>
-                <h3>{`${item.title}`}</h3>
-                <h5>{`Year released: ${item.year}`}</h5>
-                <Collapsible
+              <div className="posterContainer">
+                <img
+                  className="nominationPoster"
+                  src={item.poster}
+                  alt={item.title}
+                />
+              </div>
+              <div className="nominationTitle">{`${item.title}, ${item.year}`}</div>
+              {/* <Collapsible
                   trigger="View Plot"
                   triggerWhenOpen="Collapse Plot"
                   onOpen={() => fetchPlot(item.imdbID)}
                 >
                   <p id={`nominationplot+${item.imdbID}`}>Loading...</p>
-                </Collapsible>
-                <button onClick={(e) => handleDelete(e)}>Delete</button>
-              </div>
+                </Collapsible> */}
+              <button onClick={(e) => handleDelete(e)}>Remove</button>
             </div>
           )
         })}
