96fbf981b936eb9df7af7e9c3cebff62d27cba15
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-10 20:45:56 -0400

    feat: create a component which appear when there are no nominations to show

--- client/src/components/Nominations/Nominations.js
+++ client/src/components/Nominations/Nominations.js
@@ -15,32 +15,46 @@ const Nominations = ({
   }
 
   return (
-    <div className="allNominations">
-      {existingNominations &&
-        existingNominations.map((item) => {
-          return (
-            <div
-              id={`nomination+${item.imdbID}`}
-              key={item.imdbID}
-              className="nomination"
-            >
-              <button className="deleteButton" onClick={(e) => handleDelete(e)}>
-                <IconContext.Provider value={{ color: 'red', size: '25px' }}>
-                  <TiDelete />
-                </IconContext.Provider>
-              </button>
-              <div className="posterContainer">
-                <img
-                  className="nominationPoster"
-                  src={item.poster}
-                  alt={item.title}
-                />
-              </div>
-              <div className="nominationTitle">{`${item.title} (${item.year})`}</div>
-            </div>
-          )
-        })}
-    </div>
+    <>
+      {existingNominations.length === 0 && (
+        <div className="noNominations">
+          <h3>Nothing here yet!</h3>
+        </div>
+      )}
+      {existingNominations.length > 0 && (
+        <div className="allNominations">
+          {existingNominations &&
+            existingNominations.map((item) => {
+              return (
+                <div
+                  id={`nomination+${item.imdbID}`}
+                  key={item.imdbID}
+                  className="nomination"
+                >
+                  <button
+                    className="deleteButton"
+                    onClick={(e) => handleDelete(e)}
+                  >
+                    <IconContext.Provider
+                      value={{ color: 'red', size: '25px' }}
+                    >
+                      <TiDelete />
+                    </IconContext.Provider>
+                  </button>
+                  <div className="posterContainer">
+                    <img
+                      className="nominationPoster"
+                      src={item.poster}
+                      alt={item.title}
+                    />
+                  </div>
+                  <div className="nominationTitle">{`${item.title} (${item.year})`}</div>
+                </div>
+              )
+            })}
+        </div>
+      )}
+    </>
   )
 }
 
